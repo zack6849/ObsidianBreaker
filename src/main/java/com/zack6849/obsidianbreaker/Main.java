22110e26f81ae4ec8e7a62f0dd009704cbfb37ae
BuildTools <unconfigured@null.spigotmc.org>
2015-05-29 16:38:13 -0400

    Added reload command, fixed a bug,

--- src/main/java/com/zack6849/obsidianbreaker/Main.java
+++ src/main/java/com/zack6849/obsidianbreaker/Main.java
@@ -5,6 +5,8 @@ import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -21,12 +23,11 @@ import java.util.logging.Logger;
  * Created by Zack on 1/8/2015.
  */
 public class Main extends JavaPlugin implements Listener {
-    private Logger logger;
     private Map<Material, HashMap<String, Integer>> data = new HashMap<Material, HashMap<String, Integer>>();
 
     @Override
     public void onEnable() {
-        this.logger = getLogger();
+        Logger logger = getLogger();
         saveDefaultConfig();
         data.put(Material.BEDROCK, new HashMap<String, Integer>());
         data.get(Material.BEDROCK).put("durability", getConfig().getInt("durability.bedrock"));
@@ -34,6 +35,21 @@ public class Main extends JavaPlugin implements Listener {
         data.get(Material.OBSIDIAN).put("durability", getConfig().getInt("durability.obsidian"));
         getServer().getPluginManager().registerEvents(this, this);
         reloadConfig();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(command.getName().equalsIgnoreCase("obreaker")){
+            if(args.length == 1){
+                if(args[0].equalsIgnoreCase("reload")){
+                    reloadConfig();
+                    sender.sendMessage(ChatColor.GREEN + "Configuration reloaded!");
+                }
+            }else{
+                sender.sendMessage(ChatColor.YELLOW + "ObsidianDestroyer");
+            }
+        }
+        return false;
     }
 
     @EventHandler
@@ -52,6 +68,7 @@ public class Main extends JavaPlugin implements Listener {
                         if (b.hasMetadata("hits")) {
                             b.setMetadata("hits", new FixedMetadataValue(this, b.getMetadata("hits").get(0).asInt() + 1));
                             if (b.getMetadata("hits").get(0).asInt() >= resistance) {
+                                b.removeMetadata("hits", this);
                                 b.setType(Material.AIR);
                                 event.getLocation().getWorld().playEffect(event.getLocation(), Effect.ENDER_SIGNAL, 1);
                             }
